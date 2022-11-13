d36971d5334cca7c3a4d3115c5b43e3d0092aa2d
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-11-13 23:07:38 +0300

    Task [6kyu]: Does my number look big in this?: hotfix wrong copied code

--- python/Does my number look big in this?/solution.py
+++ python/Does my number look big in this?/solution.py
@@ -1,6 +1,8 @@
 def narcissistic(value):
-    size = len(str(value))
+    num_of_digits = len(str(value))
     result = 0
     for i in str(value):
-        result = int(i) ** size
-    return result == int(value)
+        result += int(i) ** num_of_digits
+    if result == value:
+        return True
+    return False
